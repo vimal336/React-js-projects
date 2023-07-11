3875a13928895b9b130a0ece023dce57107332db
vimal336 <asvimal336@gmail.com>
2023-07-11 19:32:00 +0530

    movie booking project

---
+++ react-movie-booking/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import MovieBooking from './components/MovieBooking'
+
+function App() {
+
+  return (
+    <div className='App'>
+
+     <MovieBooking/>
+    </div>
+     
+  )
+}
+
+export default App
