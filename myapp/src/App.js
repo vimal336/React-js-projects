032385b63a3ee6d11985572796096202e94d50d3
vimal336 <asvimal336@gmail.com>
2023-08-31 19:19:19 +0530

    react advice app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
 
-import React from 'react'
-import advice from './components/advice'
+function advice() {
+    async function getAdvice() {
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      console.log(data.slip.id, data.slip.advice);
+    }
+  
+    return (
+      <div>
+        <h1>Hello world! </h1>
+        <button onClick={getAdvice}> Get advice </button>
+      </div>
+    );
+  }
 
-export const App = () => {
-  return (
-    <div>
-
-      <advice/>
-
-    </div>
-  )
-}
-
+  export default advice;
