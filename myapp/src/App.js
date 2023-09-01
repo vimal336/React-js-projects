0686aa7b9813f8f3a7b85d0183615fd156cf0c13
vimal336 <asvimal336@gmail.com>
2023-09-01 12:50:46 +0530

    react advice app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
     const [advice, setAdvice] = useState("");
     const [adviceNumber, setadviceNumber] = useState("");
+    const [count, setCount] = useState(0);
 
     async function getAdvice() {
       const res = await fetch("https://api.adviceslip.com/advice");
       const data = await res.json();
       setAdvice(data.slip.advice);
       setadviceNumber(data.slip.id);
+      setCount((c) => c + 1);
    }
+
+   useEffect(function(){
+    getAdvice();
+   })
   
     return (
       <div> 
         <h2> ID: {adviceNumber}</h2>
         <h1> Advice :{advice}</h1>
         <button onClick={getAdvice} {...setadviceNumber}> Get advice </button>
+        <p>You have read <strong>{count}</strong> pieces of advice</p>
       </div>
     );
   }
