35f9afe663996adfc945a4a4a930228d11831020
vimal336 <asvimal336@gmail.com>
2023-09-18 20:12:29 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -2,6 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
 function App() {
   return (
     <div>
@@ -12,7 +58,9 @@ function App() {
 }
 
 function Pizza(){
-  return <h2>Pizza</h2>
+  return <div>
+    <h2>Pizza Spinaci</h2> <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+  </div> 
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
