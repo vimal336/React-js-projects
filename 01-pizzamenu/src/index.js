762fde7da3a4f39282b694b5642d56d2c9c28fe5
vimal336 <asvimal336@gmail.com>
2023-09-20 20:39:13 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -78,8 +78,19 @@ function Menu() {
       ingredients = "Tomato, mozarella, ham, aragula, and burrata cheese"
       price = "18"
       photoName = "pizzas/prosciutto.jpg"/>
-      <Pizza />
-      <Pizza />
+      <Pizza 
+       name = "Pizza Salamino"
+       ingredients = "Tomato, mozarella, and pepperoni"
+       price = '15'
+       photoName = "pizzas/salamino.jpg"
+       />
+      <Pizza 
+       name ="Pizza Funghi"
+       ingredients = "Tomato, mozarella, mushrooms, and onion"
+       price = '12'
+       photoName = "pizzas/funghi.jpg"
+      
+      />
     </main>
   );
 }
