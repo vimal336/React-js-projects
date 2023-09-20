71fd438ff14e2dbb14e6ae237cc2f6362d929b31
vimal336 <asvimal336@gmail.com>
2023-09-20 21:03:39 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -110,6 +110,7 @@ function Pizza(props) {
       <img src={props.photoName} alt={props.name} />
       <h3>{props.name}</h3>
       <p>{props.ingredients}</p>
+      <span>{props.price}</span>
     </div>
   );
 }
