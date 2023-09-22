447b01ff5f57777e66af6582521a40e8c5fb5e77
vimal336 <asvimal336@gmail.com>
2023-09-22 22:27:47 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -73,7 +73,7 @@ function Menu() {
       <div>
         { pizzaData.map ( pizza =>
         
-        <Pizza pizzaObj = {pizza}/>
+        <Pizza pizzaObj = {pizza} key = {pizza.name}/>
         
         )}
       </div>
