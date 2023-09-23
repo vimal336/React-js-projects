f6668cf21f1627a2d6b17d1636125bab86b1f904
vimal336 <asvimal336@gmail.com>
2023-09-23 22:21:03 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -103,18 +103,25 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
 
-const hour = new Date().getHours();
-const openHour = 12;
-const closeHour = 22;
-const isOpen = hour >= openHour && hour <= closeHour;
-console.log(isOpen);
-
-return <footer className="footer">
-    {isOpen && <p> we're open until {closeHour}:00. come visit us or order online. </p>}
-      </footer>
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>
+            we're open until {closeHour}:00. come visit us or order online.
+          </p>
+          <button className="btn"> Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
-  
 
 function Pizza(props) {
   return (
