8c4d2774891093acc367a24afc7249c36a1a9d38
vimal336 <asvimal336@gmail.com>
2023-09-26 20:32:21 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -136,10 +136,10 @@ function Order(props) {
   )
 }
 
-function Pizza(props) {
+function Pizza({pizzaObj}) {
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={props.pizzaObj.name} />
       <h3>{props.pizzaObj.name}</h3>
       <p>{props.pizzaObj.ingredients}</p>
       <span>{props.pizzaObj.price + 3}</span>
