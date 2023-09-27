516e061f7c6dfa65722b9cdc1016fd193ccde7a2
vimal336 <asvimal336@gmail.com>
2023-09-27 22:11:04 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -73,11 +73,13 @@ function Menu() {
     <main className="menu">
       <h2> Our menu </h2>
 
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic all delicious.
-      </p>
 
       {numpizzas > 0 && (
+
+        
+      <p>
+      Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic all delicious.
+    </p>
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
