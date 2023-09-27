e9c4f697b36f234f3291a72cd55478da4d4bee7f
vimal336 <asvimal336@gmail.com>
2023-09-27 22:21:33 +0530

    react menu project

--- 01-pizzamenu/src/index.js
+++ 01-pizzamenu/src/index.js
@@ -147,7 +147,7 @@ function Order({ openHour, closeHour }) {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
