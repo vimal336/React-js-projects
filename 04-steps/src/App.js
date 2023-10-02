774284eaabd7cd859a567faa5725f1f5a97294a4
vimal336 <asvimal336@gmail.com>
2023-10-02 22:52:12 +0530

    react steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div> 1 </div>
+        <div className="active"> 1 </div>
         <div> 2 </div>
         <div> 3 </div>
       </div>
@@ -11,7 +11,7 @@ export default function App() {
 
       <div className="buttons">
         <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-        <button>Next</button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
       </div>
     </div>
   );
