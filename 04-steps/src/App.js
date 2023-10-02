71e14b534c3069e6ca7312d2e94cdff9128ff4ab
vimal336 <asvimal336@gmail.com>
2023-10-02 22:54:45 +0530

    react steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,13 @@
+const messages = [
+  "Learn React *",
+  "Apply for jobs --",
+  "Invest your new income"
+]
+
+
+
+
+
 export default function App() {
   return (
     <div className="steps">
@@ -10,8 +20,12 @@ export default function App() {
       <p className="message">Hello</p>
 
       <div className="buttons">
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
       </div>
     </div>
   );
