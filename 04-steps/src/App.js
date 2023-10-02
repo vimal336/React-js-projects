93287555d4c48e3fe02cfb1af521738e0388970b
vimal336 <asvimal336@gmail.com>
2023-10-02 22:38:06 +0530

    react steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,8 +1,11 @@
 export default function App() {
   return (
-    <div className="App">
-    hello
-       
+    <div className="steps">
+      <div className="numbers">
+        <div> 1 </div>
+        <div> 2 </div>
+        <div> 3 </div>
+      </div>
     </div>
   );
 }
