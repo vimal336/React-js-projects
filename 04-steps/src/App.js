6f2f6d8be7362675b7126904ec38244852b777a3
vimal336 <asvimal336@gmail.com>
2023-10-06 13:50:23 +0530

    react steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -22,31 +22,39 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
+
+    <div>
+      
+   { isOpen && (
+
+      <div className="steps">
       <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}> 1 </div>
+      <div className={step >= 1 ? "active" : ""}> 1 </div>
         <div className={step >= 2 ? "active" : ""}> 2 </div>
         <div className={step >= 3 ? "active" : ""}> 3 </div>
-      </div>
+        </div>
 
-      <p className="message">
+        <p className="message">
         Step {step}: {messages[step - 1]}
-      </p>
-
-      <div className="buttons">
+        </p>
+        
+        <div className="buttons">
         <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
+        style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        onClick={handlePrevious}
         >
           Previous
-        </button>
-        <button
+          </button>
+          <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
           onClick={handleNext}
-        >
+          >
           Next
-        </button>
+          </button>
       </div>
+    </div>
+    )
+    }
     </div>
   );
 }
