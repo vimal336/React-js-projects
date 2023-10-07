62559ba27cdd2dcf34adcfd0a1627195e3fa5d10
vimal336 <asvimal336@gmail.com>
2023-10-07 22:07:04 +0530

    react steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -35,7 +35,7 @@ export default function App() {
             Step {step}: {messages[step - 1]}
           </p>
 
-          <div className="buttons">
+          <div className="buttons message">
             <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
