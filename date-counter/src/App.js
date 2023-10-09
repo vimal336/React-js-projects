3bc602b62aed519aae07be8eaeff9441b02ee16f
vimal336 <asvimal336@gmail.com>
2023-10-09 20:26:48 +0530

    date counter project

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -2,40 +2,34 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
-
   const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
 
-  
-function stepDec() {
-  setStep(step - 1)
-}
+  function stepDec() {
+    setStep(step - 1);
+  }
 
-function stepInc() {
-  setStep(step + 1)
-}
-
-
-
+  function stepInc() {
+    setStep(step + 1);
+  }
 
   return (
-   
-      <div className="App">
-        <h1>Date counter app</h1>
-        <div>
-          <button onClick={stepDec}>-</button> step : {step} <button onClick={stepInc}>+</button>
-        </div>
-
-        <div>
-          <button>-</button> count : 1 <button>+</button>
-        </div>
-        <div>
-          <p>70 days from today is Mon Aug 30 2027</p>
-        </div>
+    <div className="App">
+      <h1>Date counter app</h1>
+      <div>
+        <button onClick={stepDec}>-</button> 
+         step : {step}
+        <button onClick={stepInc}>+</button>
       </div>
-    
+
+      <div>
+        <button>-</button> count : 1 <button>+</button>
+      </div>
+      <div>
+        <p>7 days from today is Mon Aug 30 2027</p>
+      </div>
+    </div>
   );
 }
-
 
 export default App;
