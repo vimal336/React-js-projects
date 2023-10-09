0ac4ff2613c058c2ca8ded67a28e5ae0e74c4f67
vimal336 <asvimal336@gmail.com>
2023-10-09 19:27:56 +0530

    date counter project

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="App">
      <h1>date counter app</h1>
+     <div> 
 
+     <button>+</button> step  <button>+</button> 
+
+     </div>
+      
     </div>
   );
 }
