d5ad2204fb434198aa4618df58ee6ed193799b8c
vimal336 <asvimal336@gmail.com>
2023-10-09 19:31:11 +0530

    date counter project

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <>
       <h1>date counter app</h1>
       <div>
-        <button>-</button> step : <button>+</button>
+        <button>-</button> step : 1 <button>+</button>
       </div>
 
       <div>
-        <button>-</button> count : <button>+</button>
+        <button>-</button> count : 1 <button>+</button>
       </div>
     </div>
+
+    <p>70 days from today is Mon Aug 30 2027</p>
+    </>
   );
 }
 
