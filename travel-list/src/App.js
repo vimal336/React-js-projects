fe13183ed22b5deeec6c35a38ee2c35a1214339f
vimal336 <asvimal336@gmail.com>
2023-10-13 12:35:05 +0530

    travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -6,7 +6,12 @@ function Logo() {
   return <h1> 🌴 Far Away 👜 </h1>
 }
 
-function Form() {}
+function Form() {
+  return <div className="add-form"> 
+  
+  <h3> what do you need for your 😍 trip?</h3>
+  </div>
+}
 
 function PackingList() {}
 
