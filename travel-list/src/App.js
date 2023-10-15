28fa77b5abf7d16f441052b1654269e8ef458af9
vimal336 <asvimal336@gmail.com>
2023-10-15 22:10:56 +0530

    travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -24,7 +24,10 @@ function Form() {
     <form className="add-form">
       <h3> what do you need for your 😍 trip?</h3>
       <select>
-        {Array.from({length:20}, (_, i) => i+1)}
+        {Array.from({length:20}, (_, i) => i+1).map(num=><option value={num} key={num}>
+          {num}
+        </option>)
+        }
       </select>
     </form>
   );
