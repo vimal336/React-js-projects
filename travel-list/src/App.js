749ef9129be53716a574ccec50325c792390adb6
vimal336 <asvimal336@gmail.com>
2023-10-16 20:43:11 +0530

    travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -34,7 +34,7 @@ function Form() {
       <h3> what do you need for your 😍 trip?</h3>
       <select
        value={quantity}
-       onChange={(e) => setQuantity(e.target.value)}
+       onChange={(e) => setQuantity(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
