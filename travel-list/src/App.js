4edc6817ba8cd406d429bf7c86a81d27b442ae60
vimal336 <asvimal336@gmail.com>
2023-10-17 19:47:00 +0530

    travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -4,7 +4,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Charger", quantity: 1, packed: false },
-  { id: 3, description: "Powerbank", quantity: 1, packed: false },
+
 ];
 
 export default function App() {
