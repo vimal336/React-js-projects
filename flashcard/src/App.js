9ae4a61b4b425ea3df311b5563b73ce27f550c4f
vimal336 <asvimal336@gmail.com>
2023-10-27 12:48:53 +0530

    react flashcard project

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   return (
@@ -13,54 +13,56 @@ const questions = [
   {
     id: 3457,
     question: "What language is React based on?",
-    answer: "JavaScript",
+    answer: "JavaScript"
   },
   {
     id: 7336,
     question: "What are the building blocks of React apps?",
-    answer: "Components",
+    answer: "Components"
   },
   {
     id: 8832,
     question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX",
+    answer: "JSX"
   },
   {
     id: 1297,
     question: "How to pass data from parent to child components?",
-    answer: "Props",
+    answer: "Props"
   },
   {
     id: 9103,
     question: "How to give components memory?",
-    answer: "useState hook",
+    answer: "useState hook"
   },
   {
     id: 2002,
     question:
       "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element",
-  },
+    answer: "Controlled element"
+  }
 ];
 
 function FlashCards() {
-  const [selectedId, setSelectedId] = useState(9103);
+  const [selectedId, setSelectedId] = useState(null);
 
-  function handleClick(){
-
+  function handleClick(id) {
+    setSelectedId(null);
   }
 
   return (
     <div className="flashcards">
       {questions.map((question) => (
-       <div key = {question.id} 
-       onClick={() => handleClick(question.id)}
-       className={question.id === selectedId ? "selected" : ""}>
-        <p>{question.id === selectedId ? question.answer : question.question}</p>
-       </div>
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
       ))}
     </div>
-
-    
   );
 }
