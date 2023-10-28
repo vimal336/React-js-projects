495f8cf87622310d16a81182d693fc5309e5da6e
vimal336 <asvimal336@gmail.com>
2023-10-28 12:34:18 +0530

    react date counter project

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -14,6 +14,8 @@ function Counter2() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
+  const [inputValue, setInputValue] = useState(0);
+
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
