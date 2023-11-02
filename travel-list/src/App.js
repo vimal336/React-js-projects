d379f2fbc3b3cf9b60e791c0839bef371d329f48
vimal336 <asvimal336@gmail.com>
2023-11-02 22:16:07 +0530

    react travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const initialItems = [
-  { id: 1, description: "Passport", quantity: 2, packed: false },
+  { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Mobile", quantity: 1, packed: false },
 
