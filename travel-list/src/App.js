dd9a8fe3e45213b8fe626bfff6d99ed435170bcf
vimal336 <asvimal336@gmail.com>
2023-11-02 22:16:56 +0530

    react travel list project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -4,7 +4,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Mobile", quantity: 1, packed: false },
-  { id: 4, description: "Mobile", quantity: 1, packed: false },
+  { id: 4, description: "watch", quantity: 1, packed: false },
 
 ];
 
