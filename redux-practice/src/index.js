dfaf7921dd09ca8476b904d0315af4a81ed73583
vimal336 <asvimal336@gmail.com>
2023-11-13 19:45:21 +0530

    react redux

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Store } from './App/store';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
