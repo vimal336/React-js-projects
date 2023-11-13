7311006022305c43ddf1d299aa654d0b64a11558
vimal336 <asvimal336@gmail.com>
2023-11-13 19:47:23 +0530

    react redux

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Store } from './App/store';
+import { store } from './App/store'
 import { Provider } from  'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}
     <App />
   </React.StrictMode>
 );
