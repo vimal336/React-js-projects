8ac9daf78fe3e96e519d6d5ae9fdd41f9092f133
vimal336 <asvimal336@gmail.com>
2023-11-15 14:26:40 +0530

    react redux

--- redux-practice/src/features/counters/counterSlice.js
+++ redux-practice/src/features/counters/counterSlice.js
@@ -14,6 +14,9 @@ export const counterSlice = createSlice({
 
         decrement: (state) => {
             state.count -=1;
+        },
+        reset: (state) => {
+            state.count = 0;
         }
     }
 
