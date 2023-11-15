2308ce9a0b4d3bfeb3f779b2b82ecc95a1eee486
vimal336 <asvimal336@gmail.com>
2023-11-15 14:31:53 +0530

    react redux

--- redux-practice/src/features/counters/Counter.js
+++ redux-practice/src/features/counters/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
