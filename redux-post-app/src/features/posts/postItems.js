58d74d8ea59a78e20b9453944008c5b3c6c4cad2
vimal336 <asvimal336@gmail.com>
2023-11-24 22:20:39 +0530

    react redux

--- redux-post-app/src/features/posts/postItems.js
+++ redux-post-app/src/features/posts/postItems.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 const PostList = () => {
 
-const posts = useSelecter((state) => state.posts)
+const posts = useSelector((state) => state.posts)
 
 const renderedPosts = posts.map(post => (
 
