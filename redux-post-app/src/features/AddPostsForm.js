0d6dd7681def836363712d96579fbde4c758502f
vimal336 <asvimal336@gmail.com>
2023-11-26 22:15:28 +0530

    react redux

--- redux-post-app/src/features/AddPostsForm.js
+++ redux-post-app/src/features/AddPostsForm.js
@@ -1,11 +1,12 @@
 import { useState  } from "react";
 
-import React from 'react'
-
 const AddPostsForm = () => {
+
   return (
-    <div>AddPostsForm</div>
+    
   )
+
 }
+
 
 export default AddPostsForm
