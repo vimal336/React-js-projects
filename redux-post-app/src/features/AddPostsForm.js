bbd9c5718deb6794d9af6ca07621c73c97ccf9fd
vimal336 <asvimal336@gmail.com>
2023-11-27 22:21:10 +0530

    react redux

--- redux-post-app/src/features/AddPostsForm.js
+++ redux-post-app/src/features/AddPostsForm.js
@@ -9,6 +9,21 @@ const [content, setContent] = useState('')
     <section>
         <h2> Add a New Post </h2>
         <form>
+          <dl class="row">
+            <dt class="col-sm-3">Term</dt>
+            <dd class="col-sm-9">definition</dd>
+            <dt class="col-sm-3">Term</dt>
+            <dd class="col-sm-9">definition</dd>
+            <dt class="col-sm-3 text-truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit.</dt>
+            <dd class="col-sm-9">Term</dd>
+            <dt class="col-sm-3">Nesting</dt>
+            <dd class="col-sm-9">
+              <dl class="row">
+                <dt class="col-sm-4">Nested title</dt>
+                <dd class="col-sm-8">Nested definition</dd>
+              </dl>
+            </dd>
+          </dl>
           
         </form>
     </section>
