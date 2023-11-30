de1443c08d1b580cac015b51a4029302fa1a2460
vimal336 <asvimal336@gmail.com>
2023-11-30 22:21:37 +0530

    react redux

--- redux-post-app/src/features/AddPostForm.js
+++ redux-post-app/src/features/AddPostForm.js
@@ -1,57 +1,48 @@
-import { useState  } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-const dispatch = useDispatch() 
-const [title, setTitle] = useState('')
-const [content, setContent] = useState('')
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
 
-const onTitleChanged = e => setTitle(e.target.value)
-const onContentChanged = e => setContent(e.target.value)
-
-const onSavePostClicked = () =>{
-  if(title && content){
-    dispatch(
-      postAdded(
-        {
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
           id: nanoid(),
           title,
-        }
-      )
-    )
-  }
-}
+        })
+      );
+    }
+  };
   return (
     <section>
-        <h2> Add a New Post </h2>
-        <form>
-          <input
-          type = "text"
+      <h2> Add a New Post </h2>
+      <form>
+        <input
+          type="text"
           id="postTitle"
           name="postTitle"
           value={title}
           onChange={onTitleChanged}
-          />
-          <label htmlFor="postContent">Content:</label>
-          <textarea
-           id="postContent"
-           name="postContent"
-           value={content}
-           onChange={onContentChanged}
-          />
-          <button
-           type="button"
-          >
-            
-          </button>
-        </form>
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button"> Save Post</button>
+      </form>
     </section>
-  )
+  );
+};
 
-}
-
-
-export default AddPostForm
+export default AddPostForm;
