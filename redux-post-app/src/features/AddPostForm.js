5c9722ddb52ac16201cd7c9b7134ba978eb6d7ab
vimal336 <asvimal336@gmail.com>
2023-12-02 22:35:59 +0530

    react redux

--- redux-post-app/src/features/AddPostForm.js
+++ redux-post-app/src/features/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
-
+import { postAdded } from "./posts/postsSlice";
 
 const AddPostForm = () => {
   const dispatch = useDispatch()
