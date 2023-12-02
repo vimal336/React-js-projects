b2566a2533c39aca4bb6be73f44f7131250ea58a
vimal336 <asvimal336@gmail.com>
2023-12-02 22:52:01 +0530

    react redux

--- redux-post-app/src/features/posts/postList.js
+++ redux-post-app/src/features/posts/postList.js
@@ -4,7 +4,13 @@ const PostList = () => {
 
 const posts = useSelector((state) => state.posts)
 
-const renderedPosts = post.map
+const renderedPosts = posts.map(post => (
+
+  <article key={post.id}>
+    <h3>{post.title}</h3>
+    <p>{post.content.substring(0, 100)}</p>
+  </article>
+))
 
 return (
   <section>
