b6cfad65751e2e1dd17eb2c37c99150aa251865f
vimal336 <asvimal336@gmail.com>
2023-12-06 22:22:37 +0530

    react redux

--- redux-post-app/src/features/posts/postsSlice.js
+++ redux-post-app/src/features/posts/postsSlice.js
@@ -20,7 +20,7 @@ const initialState = [
         {
             id:'4',
             title:'Views',
-            content:'Likes, Shares this videos'
+            content:'Likes, Shares this video'
         }
 
 ]
