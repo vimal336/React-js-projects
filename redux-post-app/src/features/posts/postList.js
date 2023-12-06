28d5aa6eaa306e2994609244b95c1a04ed086d19
vimal336 <asvimal336@gmail.com>
2023-12-06 14:56:39 +0530

    react redux

--- redux-post-app/src/features/posts/postList.js
+++ redux-post-app/src/features/posts/postList.js
@@ -9,7 +9,8 @@ const renderedPosts = posts.map(post => (
 
   <article key={post.id}>
     <h3>{post.title}</h3>
-    <p>{post.content.substring(0, 100)}</p>
+    <p>{post.content ? post.content.substring(0, 100) : ''}</p>
+
   </article>
 ))
 
