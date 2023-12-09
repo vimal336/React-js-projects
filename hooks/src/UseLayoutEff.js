38096abcd61a05a006f132b82bf499e3fa2072f6
vimal336 <asvimal336@gmail.com>
2023-12-09 22:31:16 +0530

    react hooks

--- hooks/src/UseLayoutEff.js
+++ hooks/src/UseLayoutEff.js
@@ -10,6 +10,7 @@ import React from "react";
     useEffect(() => {
      if(popup.current == null || button.current == null) return 
      const { bottom } = button.current.getBoundingClientREct()
+     popup.current.style.top = `${button + 25}px`
       
 
     },[show]);
