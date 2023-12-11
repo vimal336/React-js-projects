726e614ca1b3a3f182613b7d9484353c3bc216c1
vimal336 <asvimal336@gmail.com>
2023-12-11 20:47:12 +0530

    react redux post app

--- redux-post-app/src/app/store.js
+++ redux-post-app/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore  } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export const store = configureStore({
     reducer:{
