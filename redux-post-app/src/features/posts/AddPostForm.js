d9c7a90e994ca7af0c931f0b2ee1e3de63faabe2
vimal336 <asvimal336@gmail.com>
2023-12-11 20:51:17 +0530

    react redux post app

--- redux-post-app/src/features/posts/AddPostForm.js
+++ redux-post-app/src/features/posts/AddPostForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
