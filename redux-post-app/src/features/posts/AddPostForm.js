55abd4994d4a4c6594d3d02a488eedb6fac97bc4
vimal336 <asvimal336@gmail.com>
2023-12-13 20:48:32 +0530

    react redux post app

--- redux-post-app/src/features/posts/AddPostForm.js
+++ redux-post-app/src/features/posts/AddPostForm.js
@@ -24,7 +24,9 @@ const AddPostForm = () => {
 
   const usersOptions = users.map(user => (
 
-    <option key={user.id} value={user.id}></option>
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
   ))
   return (
     <section>
