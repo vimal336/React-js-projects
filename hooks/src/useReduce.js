efd6c139ba59eabed2fc5efcb55fe3fecce85cbc
vimal336 <asvimal336@gmail.com>
2023-12-17 22:32:19 +0530

    react usereducer hook

--- hooks/src/useReduce.js
+++ hooks/src/useReduce.js
@@ -3,8 +3,12 @@ import React from "react";
 import { Reducer } from "react";
 
 export default function Reducers(){
+
+    const [this.state.first]
     return(
         <>
+
+
         
         </>
     )
