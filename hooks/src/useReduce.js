872fc0099b949c173bdd6efe3c7ece6d6269d89b
vimal336 <asvimal336@gmail.com>
2023-12-18 22:20:01 +0530

    react usereducer

--- hooks/src/useReduce.js
+++ hooks/src/useReduce.js
@@ -3,10 +3,12 @@ import React, { useReducer } from "react";
 import { Reducer } from "react";
 
 const transactionReducer = (state, action) =>{
-    
+
 }
 
 export default function Reducers(){
+
+    const withdraw = 
 
     const [state, dispatch] = useReducer(transactionReducer, 1000)
     return(
