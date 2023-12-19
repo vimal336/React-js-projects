7a53b78424e5ec4ddb5c6e45b4c654eb159f79f8
vimal336 <asvimal336@gmail.com>
2023-12-19 22:23:51 +0530

    react usereducer

--- hooks/src/useReduce.js
+++ hooks/src/useReduce.js
@@ -24,7 +24,7 @@ export default function Reducers(){
     const [state, dispatch] = useReducer(transactionReducer, 1000)
     return(
         <>
-       <h3></h3>
+       <h3>{}</h3>
 
         
         </>
