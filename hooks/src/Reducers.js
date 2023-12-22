517cc26b108fd92340318e0177293cbe71b42798
vimal336 <asvimal336@gmail.com>
2023-12-22 20:46:37 +0530

    react usereducer

--- hooks/src/Reducers.js
+++ hooks/src/Reducers.js
@@ -4,7 +4,7 @@ import { useReducer } from "react";
 export default function Reducers(){
 
     const withdraw = (amount) => {
-        dispatch({type:'DEPOSIT'})
+        dispatch({type:'DEPOSIT', payload:})
     }
 
     const[state, dispatch] = useReducer
