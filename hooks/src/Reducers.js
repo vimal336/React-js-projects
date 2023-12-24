5fd0d7b4bc8add54fd9b47dbbeed03dba28822cc
vimal336 <asvimal336@gmail.com>
2023-12-24 21:57:10 +0530

    react usereducer

--- hooks/src/Reducers.js
+++ hooks/src/Reducers.js
@@ -8,7 +8,7 @@ const transactionReducer = (state,action) => {
        case  'DEPOSIT'
             return state - action.payload
        default:
-            return
+            return state
     }
 }
 
