ab998b89e42827307b815d3f453538b19ae958cd
vimal336 <asvimal336@gmail.com>
2023-12-25 22:33:12 +0530

    react usereducer

--- hooks/src/Reducers.js
+++ hooks/src/Reducers.js
@@ -27,7 +27,7 @@ export default function Reducers(){
         <>
           <h3>{`balance is ${state}`}</h3>
           <button onClick={withdraw}>withdraw</button>
-          <button onClick={deposit}>withdraw</button>
+          <button onClick={deposit}>deposit</button>
         </>
     )
 }
