4b15eb5e56e74d29e7c10a1a183afaa7fe0a4faf
vimal336 <asvimal336@gmail.com>
2024-02-07 20:58:56 +0530

    react hooks

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UseLayoutEff from "./UseLayoutEff";
 import Useredu from "./useReduce";
+import { UsersCard } from "./UsersCard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      <h1>hooks</h1>
      <UseLayoutEff/>
      <Useredu/>
+     <UsersCard/>
     </main>
   );
 }
