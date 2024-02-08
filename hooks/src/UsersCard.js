4566a343318dc0f24cd631391fde255bd1c49b6f
vimal336 <asvimal336@gmail.com>
2024-02-08 20:20:42 +0530

    react hooks

--- hooks/src/UsersCard.js
+++ hooks/src/UsersCard.js
@@ -1,7 +1,7 @@
 function User(){
    return <div className="card-container">
     <span className="pro"> ONLINE</span>
-    <img src="images/man.png" alt="man" />
+    <img src="images/avatar.jpg" alt="man" />
    </div>
 }
 export const UsersCard = () => {
