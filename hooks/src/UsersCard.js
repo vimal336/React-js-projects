cd334bbf4e7abde9d173e9ad040bd39cd661b378
vimal336 <asvimal336@gmail.com>
2024-02-09 19:52:09 +0530

    react hooks

--- hooks/src/UsersCard.js
+++ hooks/src/UsersCard.js
@@ -1,5 +1,7 @@
 function User(){
-   return <div className="card-container">
+   return (
+   
+   <div className="card-container">
     <span className="pro"> ONLINE</span>
     <img src="images/prof.jfif" alt="man" />
    </div>
