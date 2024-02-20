1a2cf807ed2d3e0b67bd17bea741ee5aaa5cd186
vimal336 <asvimal336@gmail.com>
2024-02-20 20:30:10 +0530

    react hooks

--- hooks/src/UsersCard.js
+++ hooks/src/UsersCard.js
@@ -32,7 +32,7 @@ export const UsersCard = () => {
       place = "Villupuram"
       skills = {["UI/UX", "HTML", "CSS", "Javascript", "React", "Node"]}
       online = {true}
-      profile = "images/avatar.jpeg"
+      profile = "images/man.jfif"
       />;
 
     </div>
