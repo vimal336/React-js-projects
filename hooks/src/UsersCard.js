c5b1bf752e2b600354af4a038f88dfffbf56f15c
vimal336 <asvimal336@gmail.com>
2024-02-21 19:16:23 +0530

    react hooks

--- hooks/src/UsersCard.js
+++ hooks/src/UsersCard.js
@@ -3,7 +3,7 @@ const userData = [
     name : 'Vimal S',
     city : "Villupuram",
     role : 'Front-end Developer',
-    skills : ["UI/UX","Front-end Development", "HTML"]
+    skills : ["UI/UX","Front-end Development", "HTML", "CSS"]
   },
   {
 
