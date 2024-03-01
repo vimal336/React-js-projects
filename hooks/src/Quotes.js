31b15d8038db68f3ce69b85aca1863b8c31dc599
vimal336 <asvimal336@gmail.com>
2024-03-02 05:13:32 +0530

    react advce app

--- hooks/src/Quotes.js
+++ hooks/src/Quotes.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Quotes = () => {
-const [Advice, setAdvice] = useState("Get Quote");
+  const [Advice, setAdvice] = useState("Get Quote");
 
-  async function getAdvice(){
-    const res = await fetch()
+  async function getAdvice() {
+    const res = await fetch();
     const data = await res.json();
     console.log(data);
   }
 
   return (
     <div>
-       <button onClick={getAdvice}>{Advice}</button> 
+      <button onClick={getAdvice}>{Advice}</button>
     </div>
-  )
-}
+  );
+};
 
 export default Quotes;
