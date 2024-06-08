1231ed00d2d0562fddef6590218d8c33d65875b9
vimal336 <asvimal336@gmail.com>
2024-06-08 22:03:59 +0530

    react practice

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,27 +1,20 @@
 import { Header } from "./components/Header";
 import { Component } from "react";
 
-
-
 function App() {
+  const data = {
+    name: "rock",
+    age: 20,
+    job: "teacher",
+  };
 
-    const data = {
-        name:"rock",
-        age:20,
-        job:"teacher",
-    }
-
-
-    return(
-        <>
-<Header header = "header" value = "hello"/>
-<Header value = "value" />
-<Component/>
-</>
-    )
-
+  return (
+    <>
+      <Header name= value="hello" />
+      <Header value="value" />
+      <Component />
+    </>
+  );
 }
-
-
 
 export default App;
