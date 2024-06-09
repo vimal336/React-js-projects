d59bc8e1f497c279055e7cecb1ef5658a436c6ed
vimal336 <asvimal336@gmail.com>
2024-06-09 10:34:35 +0530

    react practice

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -4,7 +4,7 @@ export const Header = (props) => {
   return (
     <div>
         <h1 style={{backgroundColor:'red'}}>{props.name}</h1>
-        <h1>{props.age},{props.job}</h1>
+        <h1>{props.age},"profession"{props.job}</h1>
     </div>
   )
 }
