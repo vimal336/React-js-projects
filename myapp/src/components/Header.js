ff3bc2580d11cbb8686fefff8d328d5c868ee548
vimal336 <asvimal336@gmail.com>
2024-06-10 10:34:01 +0530

    react practice

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Header = (props) => {
+  
+  const [increment, setincrement] = useState(0);
+
+  const handleincrement = () =>{
+    setincrement(c => c+1)
+  }
+
   return (
     <div>
+          <h1>{increment}</h1>
+         <button onClick={handleincrement}> </button>
         <h1 style={{backgroundColor:'red'}}>{props.name}</h1>
         <h1>Age:{props.age}Profession:{props.job}</h1>
     </div>
