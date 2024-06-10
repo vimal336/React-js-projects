fccbf656d102e6125514ca73f912ced1e355a687
vimal336 <asvimal336@gmail.com>
2024-06-10 15:25:18 +0530

    react practice

--- myapp/src/components/Useeffecthook.js
+++ myapp/src/components/Useeffecthook.js
@@ -1,3 +1,28 @@
+
+// syntax
+
+// useEffect(()=>{},[])
+
+
+// 1. No dependency passed:
+// useEffect(() => {
+//Runs on every render
+// });
+
+
+// 2. An empty array:
+// useEffect(() => {
+// Runs only on the first render
+// }, []);
+
+
+// 3. Props or state values:
+// useEffect(() => {
+//Runs on the first render
+//And any time any dependency value changes
+// }, [prop, state]);
+
+
 import { useEffect, useState } from "react"
 
 export const Useeffecthook = () =>{
