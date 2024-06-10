c61eb7a722623e82b202112d46bdb2ab545549ab
vimal336 <asvimal336@gmail.com>
2024-06-10 10:40:38 +0530

    react practice

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -5,13 +5,19 @@ export const Header = (props) => {
   const [increment, setincrement] = useState(0);
 
   const handleincrement = () =>{
-    setincrement(c => c+1)
+    setincrement(c => c + 1)
   }
+
+  const handledecrement = () =>{
+    setincrement(c => c - 1)
+  }
+
 
   return (
     <div>
           <h1>{increment}</h1>
          <button onClick={handleincrement}> + </button>
+         <button onClick={handledecrement}> - </button>
         <h1 style={{backgroundColor:'red'}}>{props.name}</h1>
         <h1>Age:{props.age}Profession:{props.job}</h1>
     </div>
