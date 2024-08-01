9889c427ca6b93af3888b1e57acf6f600b52ab4c
vimal336 <asvimal336@gmail.com>
2024-08-01 11:53:43 +0530

    react useefect hook

--- hooks/src/Reducers.js
+++ hooks/src/Reducers.js
@@ -1,33 +1,10 @@
 import React from "react";
 import { useReducer } from "react";
 
-const transactionReducer = (state,action) => {
-    switch(action.type) {
-       case 'WITHDRAW':
-            return state - action.payload
-       case  'DEPOSIT' :
-            return state + action.payload
-       default:
-            return state
-    }
-}
-
-export default function Reducers(){
-
-    const withdraw = (amount) => {
-        dispatch({type:'WITHDRAW', payload:amount})
-    }
-
-    const deposit = (amount) => {
-        dispatch({type:'DEPOSIT', payload:amount})
-    }
-
-    const[state, dispatch] = useReducer(transactionReducer,1000)
+export const Todos = (state,action) => {
+    
     return(
-        <>
-          <h3>{`balance is ${state}`}</h3>
-          <button onClick={withdraw(500)}>withdraw</button>
-          <button onClick={deposit(500)}>deposit</button>
-        </>
+        <></>
     )
+
 }
