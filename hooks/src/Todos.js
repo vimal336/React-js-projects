428d493184f0ace6e25284f4a1b8aaf1e6611b1b
vimal336 <asvimal336@gmail.com>
2024-08-01 15:38:55 +0530

    react usereducer hook

--- hooks/src/Todos.js
+++ hooks/src/Todos.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { useReducer } from "react";
 
+const initialState = [
+  {
+    id:1,
+    name:"Learning React"
+  }
+];
 
-
-const initialState = [];
-
- const reducer = (state,action) =>{
-    switch(){
-        
-    }
-
- }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      break;
+    default:
+      return state;
+  }
+};
 
 export const Todos = () => {
-    
-    const [state, dispatch] = useReducer(reducer, initialState)
-    
-    return(
-        <>
-        <h4>Todo App using UseReducer</h4>
-        </>
-    )
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-}
+  return (
+    <>
+      <h4>Todo App using UseReducer</h4>
+       <h4>Task List {state.length}</h4>
+    </>
+  );
+};
