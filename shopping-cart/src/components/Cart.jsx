95def9c70e04acc9f7a335e47a06903abbd7c91e
vimal336 <asvimal336@gmail.com>
2024-08-11 19:28:43 +0530

    react redux

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,7 +1,25 @@
 export const Cart = () => {
   return (
     <div className="cart-container">
-       <div className="cart-product">
+    <div className="cart-product">
+       <div className="img">
+      <img src="" alt="image" />
+    </div>
+    <div className="cart-product-details">
+      <h3>Product Name</h3>
+      <p>Price Rs: 45.00</p>
+    </div>
+    </div>
+    <div className="cart-product">
+       <div className="img">
+      <img src="" alt="image" />
+    </div>
+    <div className="cart-product-details">
+      <h3>Product Name</h3>
+      <p>Price Rs: 45.00</p>
+    </div>
+    </div>
+    <div className="cart-product">
        <div className="img">
       <img src="" alt="image" />
     </div>
