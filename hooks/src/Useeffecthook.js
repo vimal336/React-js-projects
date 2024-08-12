6e2f1c0e48e41965618aeb62dc117cf8f3ab9644
vimal336 <asvimal336@gmail.com>
2024-08-12 15:23:24 +0530

    react useffect

--- hooks/src/Useeffecthook.js
+++ hooks/src/Useeffecthook.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Useeffecthook  = () =>{
 
   const [even, setEven] = useState(0);
+  const [time, setTime] = useState(0);
+
+  // No Dependencies
+
+  // useEffect(()=>{
+  //   console.log("No Dependency")
+  // });
+
+  //Empty Dependency Array
+
+  //useEffect(()=>{
+  //console.log("Empty Dependency Array")
+  //},[]);
+
+  
+  //State Dependency Array
+
+  useEffect(()=>{
+  console.log("State Dependency Array")
+  },[even]);
 
   const addeven = () => {
     setEven((ev)=>(ev)+2)
@@ -12,12 +32,18 @@ export const Useeffecthook  = () =>{
     setEven((o)=>(o)+1)
   }
 
+  const addtime = () => {
+    setTime((o)=>(o)+1)
+  }
+
   return(
  <>
   <h1>Useffect Hook</h1>
   <p>{even}</p>
+  <p>{time}</p>
   <button onClick={addodd}>Odd</button>
   <button onClick={addeven}>Even</button>
+  <button onClick={addtime}>T</button>
  </>
   )
 }
