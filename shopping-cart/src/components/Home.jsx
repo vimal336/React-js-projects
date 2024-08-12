2c93f0c099e2291234f13a1d7a6c5a953c8e844a
vimal336 <asvimal336@gmail.com>
2024-08-12 12:04:58 +0530

    react redux

--- shopping-cart/src/components/Home.jsx
+++ shopping-cart/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const [products] = useState(data);
   return (
     <div className="product-container">
-      {products.map((product)=>(
+      {products.map(()=>(
         <Product key={product.id} product = {product}/>
       ))}
     </div>
