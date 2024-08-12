3ec2d0196b58a41ba8aa77a8b25b73ade8147766
vimal336 <asvimal336@gmail.com>
2024-08-12 13:12:21 +0530

    react redux

--- hooks/src/Useeffecthook.js
+++ hooks/src/Useeffecthook.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Useeffecthook  = () =>{
+
+  const [even, setEven] = useState(0);
+
+  const addeven = () => {
+    setEven((ev)=>(ev)+1)
+  }
 
   return(
  <>
   <h1>Useffect Hook</h1>
+  <p>{even}</p>
+  <button onClick={addeven}>+</button>
  </>
   )
 }
