3ef1484d772390e27e142c5c890ba83b92a1926f
vimal336 <asvimal336@gmail.com>
2024-08-16 21:13:44 +0530

    crud task

--- hooks/src/UseCallBack.js
+++ hooks/src/UseCallBack.js
@@ -14,12 +14,22 @@ import React from "react";
 import { useReducer } from "react";
 
 
-initialstate = 0
+const initialstate = {count:0};
+
+function counter(state,action){
+  switch(state.action){
+    case 'increment':
+    return {count:state.count+1}
+  default:
+  return state;
+  }
+
+}
 
 const UseCallBack = () =>{
 
 
-  const [state,dispatch] = useReducer(Counter, initialstate)
+  const [state,dispatch] = useReducer(counter, initialstate)
   
   
   return(
