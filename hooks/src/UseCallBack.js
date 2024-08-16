a86bd2d5bb984a4ee4447cf84f64b3e8f9895052
vimal336 <asvimal336@gmail.com>
2024-08-16 21:03:47 +0530

    crud task

--- hooks/src/UseCallBack.js
+++ hooks/src/UseCallBack.js
@@ -1,11 +1,15 @@
-import React from 'react'
+// import React from 'react'
 
-export const UseCallBack = () => {
-  return (
-    <div>
-        <h4>
-        UseCallBack
-        </h4>
-    </div>
-  )
-}
+// export const UseCallBack = () => {
+//   return (
+//     <div>
+//         <h4>
+//         UseCallBack
+//         </h4>
+//     </div>
+//   )
+// }
+import React from "react";
+
+import { useReducer } from "react";
+
