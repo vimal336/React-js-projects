3de139fdeef4be5504ffa90e074598f2eb88bd13
vimal336 <asvimal336@gmail.com>
2024-08-17 18:00:01 +0530

    crud task

--- hooks/src/UseCallBack.js
+++ hooks/src/UseCallBack.js
@@ -1,16 +1,26 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React, { useReducer } from "react";
 
 const  initialState = {count:0};
 
-function Counter(){
-  
+function Counter(state,action){
+ switch(action.type){
+    case "inc":
+    return {count : state.count+1}
+    default :
+    return {count:state}
+ }
 }
 
 export const UseCallBack = () => {
   const [state,dispatch] = useReducer(Counter,initialState)
+  const inc = ()=>{
+    dispatch({type:'inc'})
+  }
   return (
     <div>
-      
+      <h1>{state.count}</h1>
+      <button onClick={inc}>+</button>
     </div>
   )
 }
