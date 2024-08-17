b013953b022f4bb29b8e6a0dc51ba49e091cdc15
vimal336 <asvimal336@gmail.com>
2024-08-17 17:41:18 +0530

    crud task

--- hooks/src/useReduce.js
+++ hooks/src/useReduce.js
@@ -1,6 +1,5 @@
-import { useReducer } from "react";
-
-import React from "react";
+import React, { useReducer } from "react";
+import { IncrementButton, DecrementButton } from "./Buttons"; // Adjust the import path if necessary
 
 const initialState = { count: 0 };
 
@@ -15,16 +14,19 @@ function Counter(state, action) {
   }
 }
 
-export const  UseReduce = () => {
+export const UseReduce = () => {
   const [state, dispatch] = useReducer(Counter, initialState);
 
+  const handleIncrement = () => dispatch({ type: 'increment' });
+  const handleDecrement = () => dispatch({ type: 'decrement' });
+
   return (
-    <div className="use-reduce-container" >
-      <p className="use-reduce-count" >UseReducer Count: {state.count}</p>
+    <div className="use-reduce-container">
+      <p className="use-reduce-count">UseReducer Count: {state.count}</p>
       <div className="use-reduce-buttons">
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+        <IncrementButton onIncrement={handleIncrement} />
+        <DecrementButton onDecrement={handleDecrement} />
       </div>
     </div>
   );
-}
+};
