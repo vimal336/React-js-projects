70c357e11a83a7123ab94046fc49f4e1c47b21a8
vimal336 <asvimal336@gmail.com>
2024-08-23 15:53:41 +0530

    crud task

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="Cart" element={<Cart/>} />
+        <Route path="Cart" element={<Cart/>} />
         </Routes>
        </div>
       </BrowserRouter>
