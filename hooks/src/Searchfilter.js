49db02c8ed5684c9f1a2f7483b29ef0cef8be5ee
vimal336 <asvimal336@gmail.com>
2024-09-19 20:18:06 +0530

    react search filter

--- hooks/src/Searchfilter.js
+++ hooks/src/Searchfilter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const SearchFilter = () => {
-
+  const [query, setQuery] = useState('');
   const [data, setData] = useState([
     'Apple',
     'Banana',
